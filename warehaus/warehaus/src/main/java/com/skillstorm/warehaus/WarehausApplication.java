ff921c1b8dc9c9a520bc13e65b1ae49aa248b078
unknown <acsmithwt@gmail.com>
2023-07-06 14:13:41 -0400

    Initial Set up

---
+++ warehaus/warehaus/src/main/java/com/skillstorm/warehaus/WarehausApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.warehaus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehausApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehausApplication.class, args);
+	}
+
+}
---
+++ warehaus/warehaus/src/test/java/com/skillstorm/warehaus/WarehausApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skillstorm.warehaus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehausApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
