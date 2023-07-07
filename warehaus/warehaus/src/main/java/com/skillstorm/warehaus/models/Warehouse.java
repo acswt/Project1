4d3046c4d5a335b620fd5fb0dcc33ecbd5ccd5fc
unknown <acsmithwt@gmail.com>
2023-07-07 17:16:34 -0400

    warehouse model update

--- warehaus/warehaus/src/main/java/com/skillstorm/warehaus/models/Warehouse.java
+++ warehaus/warehaus/src/main/java/com/skillstorm/warehaus/models/Warehouse.java
@@ -23,13 +23,17 @@ public class Warehouse {
     @Column(name = "warehouse_location")
     private String warehouseLocation;
 
+    @Column(name = "warehouse_limit")
+    private int warehouseLimit;
+
     public Warehouse() {
     }
 
-    public Warehouse(int warehouse_id, String warehouseName, String warehouseLocation) {
+    public Warehouse(int warehouse_id, String warehouseName, String warehouseLocation, int warehouseLimit) {
         this.warehouse_id = warehouse_id;
         this.warehouseName = warehouseName;
         this.warehouseLocation = warehouseLocation;
+        this.warehouseLimit = warehouseLimit;
     }
 
     public int getWarehouse_id() {
@@ -56,12 +60,22 @@ public class Warehouse {
         this.warehouseLocation = warehouseLocation;
     }
 
+    public int getWarehouseLimit() {
+        return warehouseLimit;
+    }
+
+    public void setWarehouseLimit(int warehouseLimit) {
+        this.warehouseLimit = warehouseLimit;
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + warehouse_id;
         result = prime * result + ((warehouseName == null) ? 0 : warehouseName.hashCode());
+        result = prime * result + ((warehouseLocation == null) ? 0 : warehouseLocation.hashCode());
+        result = prime * result + warehouseLimit;
         return result;
     }
 
@@ -81,12 +95,22 @@ public class Warehouse {
                 return false;
         } else if (!warehouseName.equals(other.warehouseName))
             return false;
+        if (warehouseLocation == null) {
+            if (other.warehouseLocation != null)
+                return false;
+        } else if (!warehouseLocation.equals(other.warehouseLocation))
+            return false;
+        if (warehouseLimit != other.warehouseLimit)
+            return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "Warehouse [warehouse_id=" + warehouse_id + ", warehouseName=" + warehouseName + "]";
+        return "Warehouse [warehouse_id=" + warehouse_id + ", warehouseName=" + warehouseName + ", warehouseLocation="
+                + warehouseLocation + ", warehouseLimit=" + warehouseLimit + "]";
     }
+
+    
 
 }
