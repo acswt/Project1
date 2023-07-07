b5b9b7a05539a6b659932c7837dea25838a923f8
unknown <acsmithwt@gmail.com>
2023-07-07 17:12:38 -0400

    product model

--- warehaus/warehaus/src/main/java/com/skillstorm/warehaus/models/Product.java
+++ warehaus/warehaus/src/main/java/com/skillstorm/warehaus/models/Product.java
@@ -19,4 +19,78 @@ public class Product {
     @Column(name = "product_name")
     private String productName;
 
+    @Column(name = "quantity")
+    private int quantity;
+
+    public Product() {
+    }
+
+    public Product(int id, String productName, int quantity) {
+        this.id = id;
+        this.productName = productName;
+        this.quantity = quantity;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((productName == null) ? 0 : productName.hashCode());
+        result = prime * result + quantity;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Product other = (Product) obj;
+        if (id != other.id)
+            return false;
+        if (productName == null) {
+            if (other.productName != null)
+                return false;
+        } else if (!productName.equals(other.productName))
+            return false;
+        if (quantity != other.quantity)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [id=" + id + ", productName=" + productName + ", quantity=" + quantity + "]";
+    }
+
+    
+
 }
