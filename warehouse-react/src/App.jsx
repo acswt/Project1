44ed8598433ade42d5ccccb76fffb0ba7e1b7dbe
unknown <acsmithwt@gmail.com>
2023-07-11 17:47:22 -0400

    Added Components / Changes App

--- warehouse-react/src/App.jsx
+++ warehouse-react/src/App.jsx
@@ -1,30 +1,15 @@
+import { useState } from "react";
 
-import reactLogo from './assets/react.svg'
-import './App.css'
 
-function App() {
+export default function App() {
 
-  return (
-    <>
+    const [shouldRender, setShouldRender] = useState(true);
 
-    <header>
-      <h1>Welcome to the Warehouse Manager</h1>
-      </header>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div id="main-page-route-links">
+    return (
+        <>
 
-        <a class="main-page-anchors" href="">Home</a>
-        <a class="main-page-anchors" href="">Products</a>
-        <a class="main-page-anchors" href="">Warehouses</a>
-        <a class="main-page-anchors" href="">Inventory</a>
-
-      </div>
-    </>
-  )
+        </>
+    );
 }
 
-export default App
+
